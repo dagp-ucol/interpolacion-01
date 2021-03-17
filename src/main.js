0711bc03119199a9ff72c0abfb1d6c854cb8cfe4
dagp-ucol <dgentner@ucol.mx>
2021-03-17 01:04:38 -0600

    fixed bugs and added code to buttons

--- src/main.js
+++ src/main.js
@@ -8,13 +8,27 @@
 // console.log(test01.interLineal(5521500000,521500,54546,8524,552))
 
 function interLineal() {
-    let x = 1;
     let equis = Number(prompt('Introduce X -> (Valor a Calcular)'))
+    if (Number.isNaN(equis) === true || equis === undefined) {
+        return alert('DATO NO VALIDO');
+    }
     let equisCero = Number(prompt('Introduce X0 -> (Valor ya conocido)'))
+    if (Number.isNaN(equisCero) === true || equis === undefined) {
+        return alert('DATO NO VALIDO');
+    }
     let equisUno = Number(prompt('Introduce X1 -> (Valor ya conocido)'))
+    if (Number.isNaN(equisUno) === true || equis === undefined) {
+        return alert('DATO NO VALIDO');
+    }
     let efeEquisCero = Number(prompt('Introduce FX0 -> (Valor ya conocido)'))
+    if (Number.isNaN(efeEquisCero) === true || equis === undefined) {
+        return alert('DATO NO VALIDO');
+    }
     let efeEquisUno= Number(prompt('Introduce FX1 -> (Valor ya conocido)'))
-
+    if (Number.isNaN(efeEquisUno) === true || equis === undefined) {
+        return alert('DATO NO VALIDO');
+    }
+    
     let resultado = Number(((efeEquisCero)+(((efeEquisUno)-(efeEquisCero))/((equisUno)-(equisCero)))*((equis)-(equisCero))));
     console.log(resultado);
     
@@ -23,4 +37,26 @@ function interLineal() {
         } else {
             alert(`El resultado es ${resultado}`);
         }
+}
+
+
+
+function interCuadratica() {
+    noDisponible()     
+}
+
+
+
+function interLagrangeUNO() {
+    noDisponible()
+}
+
+
+
+function interLagrangeDOS() {
+    noDisponible()
+}
+
+function noDisponible() {
+    alert('Operación NO Disponible')
 }
